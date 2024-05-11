5341c8ac8907e6f3f838fd11a20f3637550b308b
Khrystyna Derenivska <khrisderenivska@gmail.com>
2024-05-11 20:56:11 +0300

    add action button to calculate profit section

--- src/pages/trading-bots/ui/calculate-profit/ui.tsx
+++ src/pages/trading-bots/ui/calculate-profit/ui.tsx
@@ -1,18 +1,34 @@
-import { Stack, rem } from "@mantine/core";
+import { useResize } from "@/hooks/useResize";
+import { Button, Stack, rem } from "@mantine/core";
 
-import { Container } from "@/shared/ui";
+import { Container, StartTradingIcon } from "@/shared/ui";
 
 import { HighlightedBlueTitle } from "../shared/HighlightedBlueTitle";
 import { ProfitChart } from "./ProfitChart/ui";
 import classes from "./styles.module.css";
 
 export const CalculateProfit = () => {
+  const { isAdaptive: md } = useResize(1200);
+
   return (
     <Stack className={classes.wrapper}>
       <Container>
         <Stack gap={rem(64)}>
           <HighlightedBlueTitle title="Calculate" subTitle="Your Profit" />
           <ProfitChart />
+          <Button
+            variant="radial-gradient"
+            style={{
+              width: md ? "344px" : "400px",
+              height: md ? "92px" : "124px",
+              fontSize: md ? "18px" : "20px",
+              margin: "0 auto",
+            }}
+            className={classes.mainButton}
+            rightSection={<StartTradingIcon />}
+          >
+            ACTIVATE BOT
+          </Button>
         </Stack>
       </Container>
     </Stack>
