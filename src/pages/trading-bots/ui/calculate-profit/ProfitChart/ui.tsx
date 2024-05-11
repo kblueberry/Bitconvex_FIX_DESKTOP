55acf793945927c61a3be6a0de3e1e2aa9b3f04f
Khrystyna Derenivska <khrisderenivska@gmail.com>
2024-05-11 20:51:33 +0300

    fix chart table height & show sliders with calculations

--- src/pages/trading-bots/ui/calculate-profit/ProfitChart/ui.tsx
+++ src/pages/trading-bots/ui/calculate-profit/ProfitChart/ui.tsx
@@ -8,31 +8,40 @@ export const ProfitChart = () => {
   return (
     <div>
       <div className={classes.calculationsWrapper}>
-        <Slider
-          className={classes.slider}
-          color="#625ff8"
-          size="xl"
-          marks={[
-            { value: 20, label: "20$" },
-            { value: 5000, label: "5000$" },
-          ]}
-          min={20}
-        />
-        <Slider
-          className={classes.slider}
-          color="#625ff8"
-          size="xl"
-          marks={[
-            { value: 20, label: "20$" },
-            { value: 5000, label: "5000$" },
-          ]}
-          min={20}
-        />
-        <Text>Profit: 140$</Text>
+        <div className={classes.sliderWrap}>
+          <Slider
+            className={classes.slider}
+            size="xl"
+            marks={[
+              { value: 20, label: "20$" },
+              { value: 5000, label: "5000$" },
+            ]}
+            min={20}
+            max={5000}
+          />
+          <Text variant="text-2">Your deposit</Text>
+        </div>
+        <div className={classes.sliderWrap}>
+          <Slider
+            className={classes.slider}
+            size="xl"
+            marks={[
+              { value: 1, label: "1 day" },
+              { value: 50, label: "50 days" },
+            ]}
+            min={1}
+            max={50}
+          />
+          <Text variant="text-2">Length of investment</Text>
+        </div>
+
+        <Text className={classes.profitMain} variant="text-1">
+          Profit: <Text className={classes.profit}>140$</Text>
+        </Text>
       </div>
       <Stack gap={rem("27px")}>
         <div id="chart" className={classes.chartTable}>
-          <ReactApexChart options={chart.options} series={chart.series} type="line" height={350} />
+          <ReactApexChart options={chart.options} series={chart.series} type="line" height={512} />
         </div>
         <div id="html-dist"></div>
       </Stack>
