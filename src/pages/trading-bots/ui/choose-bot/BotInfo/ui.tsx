c4d7caa0add8861c5464508fe70434455562f785
Khrystyna Derenivska <khrisderenivska@gmail.com>
2024-05-17 19:50:15 +0300

    forbid text selection on trade type and bot container

--- src/pages/trading-bots/ui/choose-bot/BotInfo/ui.tsx
+++ src/pages/trading-bots/ui/choose-bot/BotInfo/ui.tsx
@@ -1,6 +1,7 @@
 import { Box, Group, Stack, Text, Title, rem } from "@mantine/core";
 import clsx from "clsx";
 
+import commonClasses from "../../../styles.module.css";
 import { BotProps } from "../ui";
 import classes from "./styles.module.css";
 
@@ -8,12 +9,20 @@ export const BotInfo = ({ title, description, benefit }: BotProps) => {
   return (
     <Stack gap={rem("16px")} className={classes.botWrapper} justify={"space-between"}>
       <Group gap={rem("12px")}>
-        <Title fz={{ 0: 22, md: 24 }} order={4} className={clsx(classes.botTitle, { [classes.long]: title === "Technical Analysis Trading Bot" })}>
+        <Title
+          fz={{ 0: 22, md: 24 }}
+          order={4}
+          className={clsx(classes.botTitle, commonClasses.userSelectForbidden, { [classes.long]: title === "Technical Analysis Trading Bot" })}
+        >
           {title}
         </Title>
-        <Text className={clsx(classes.botInfoText, { [classes.scalpingBotInfo]: title === "Scalping Trading Bot" })}>{description}</Text>
+        <Text
+          className={clsx(classes.botInfoText, commonClasses.userSelectForbidden, { [classes.scalpingBotInfo]: title === "Scalping Trading Bot" })}
+        >
+          {description}
+        </Text>
         <Box className={classes.botBenefitContainer}>
-          <Text className={classes.botInfoText}>Profit for last month amounted to {benefit}</Text>
+          <Text className={clsx(commonClasses.userSelectForbidden, classes.botInfoText)}>Profit for last month amounted to {benefit}</Text>
         </Box>
       </Group>
     </Stack>
--- src/pages/trading-bots/ui/trade-type/TradeTypeWrapper/ui.tsx
+++ src/pages/trading-bots/ui/trade-type/TradeTypeWrapper/ui.tsx
@@ -2,6 +2,7 @@ import { Group, Stack, Text, Title, rem } from "@mantine/core";
 import clsx from "clsx";
 import { useState } from "react";
 
+import commonStyles from "../../../styles.module.css";
 import { TradeTypeProps } from "../ui";
 import classes from "./styles.module.css";
 
@@ -20,16 +21,16 @@ export const TradeTypeWrapper = ({ title, profit, minInvestment, risk, selected 
       onClick={selectType}
     >
       <Group gap={rem("12px")}>
-        <Title fz={{ 0: 22, md: 24 }} order={4} className={classes.tradeTitle}>
+        <Title fz={{ 0: 22, md: 24 }} order={4} className={clsx(classes.tradeTitle, commonStyles.userSelectForbidden)}>
           {title}
         </Title>
-        <Text className={classes.tradeInfoText}>
+        <Text className={clsx(classes.tradeInfoText, commonStyles.userSelectForbidden)}>
           <b className={classes.subTitle}>Profit:</b> up to {profit}% per day
         </Text>
-        <Text className={classes.tradeInfoText}>
-          <b className={classes.subTitle}>Min. investment:</b> {minInvestment}%
+        <Text className={clsx(classes.tradeInfoText, commonStyles.userSelectForbidden)}>
+          <b className={classes.subTitle}>Min. investment:</b> {minInvestment}$
         </Text>
-        <Text className={classes.tradeInfoText}>
+        <Text className={clsx(classes.tradeInfoText, commonStyles.userSelectForbidden)}>
           <b className={classes.subTitle}>Risk:</b> {risk}%
         </Text>
       </Group>
