8845bba9a2d1ff6e0d783cc39ab9675729726a79
Khrystyna Derenivska <khrisderenivska@gmail.com>
2024-05-17 20:27:44 +0300

    add box shadow & change font size and weight on Activate bot  action

--- src/pages/trading-bots/ui/calculate-profit/ui.tsx
+++ src/pages/trading-bots/ui/calculate-profit/ui.tsx
@@ -8,25 +8,13 @@ import { ProfitChart } from "./ProfitChart/ui";
 import classes from "./styles.module.css";
 
 export const CalculateProfit = () => {
-  const { isAdaptive: md } = useResize(1200);
-
   return (
     <Stack className={classes.wrapper}>
       <Container>
         <Stack gap={rem(64)}>
           <HighlightedBlueTitle title="Calculate" subTitle="Your Profit" />
           <ProfitChart />
-          <Button
-            variant="radial-gradient"
-            style={{
-              width: md ? "344px" : "400px",
-              height: md ? "92px" : "124px",
-              fontSize: md ? "18px" : "20px",
-              margin: "0 auto",
-            }}
-            className={classes.mainButton}
-            rightSection={<StartTradingIcon />}
-          >
+          <Button variant="radial-gradient" className={classes.mainButton} rightSection={<StartTradingIcon />}>
             ACTIVATE BOT
           </Button>
         </Stack>
