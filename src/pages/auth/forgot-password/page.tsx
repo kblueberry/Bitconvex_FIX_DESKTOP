49f5474a763a1ba2ffde99cc81a90bbbb681208c
Khrystyna Derenivska <khrisderenivska@gmail.com>
2024-05-18 12:47:59 +0300

    Sign in and Password recovery pages - fix buntton hover and active states

--- src/pages/auth/forgot-password/page.tsx
+++ src/pages/auth/forgot-password/page.tsx
@@ -68,7 +68,7 @@ export const Page = () => {
               </label>
             </Stack>
 
-            <Button className={classes.btn} type="button" size="xxl" variant="radial-gradient" rightSection={<ResetIcon />} onClick={onFormSubmit}>
+            <Button type="button" size="xxl" variant="radial-gradient" rightSection={<ResetIcon />} onClick={onFormSubmit}>
               Reset Password
             </Button>
             <Text c="white" ta="left" fz={16}>
