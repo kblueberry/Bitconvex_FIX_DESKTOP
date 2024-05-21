d95621a8de5baa3ee415c85ac02a3470325a8280
Khrystyna Derenivska <khrisderenivska@gmail.com>
2024-05-21 17:36:59 +0300

    main page - add margins to section titles and sub titles

--- src/pages/main/ui/collaborators/ui.tsx
+++ src/pages/main/ui/collaborators/ui.tsx
@@ -1,7 +1,8 @@
 import { Box, Image, Stack, Text, Title } from "@mantine/core";
-import { motion } from "framer-motion";
-import classes from "./styles.module.css";
 import clsx from "clsx";
+import { motion } from "framer-motion";
+
+import classes from "./styles.module.css";
 
 const COLLABORATORS = [
   `${import.meta.env.BASE_URL}assets/partners/Near.png`,
@@ -11,18 +12,20 @@ const COLLABORATORS = [
   `${import.meta.env.BASE_URL}assets/partners/Binance Labs.png`,
 ];
 
-COLLABORATORS.push(...COLLABORATORS.map(value => value + '?copy'));
+COLLABORATORS.push(...COLLABORATORS.map((value) => value + "?copy"));
 
-export const Collaborators = ({data}:{data?:string[]}) => {
-  const collaboratorsSlides = data ? data.map((url) => (
-    <li key={url}>
-      <Image src={url} alt='Partner' className={classes.collaboratorsCarouselSlide}/>
-    </li>
-  )) :  COLLABORATORS.map((url) => (
-    <li key={url}>
-      <Image src={url} alt='Partner' className={classes.collaboratorsCarouselSlide}/>
-    </li>
-  ));
+export const Collaborators = ({ data }: { data?: string[] }) => {
+  const collaboratorsSlides = data
+    ? data.map((url) => (
+        <li key={url}>
+          <Image src={url} alt="Partner" className={classes.collaboratorsCarouselSlide} />
+        </li>
+      ))
+    : COLLABORATORS.map((url) => (
+        <li key={url}>
+          <Image src={url} alt="Partner" className={classes.collaboratorsCarouselSlide} />
+        </li>
+      ));
   return (
     <motion.div
       className={classes.rateWrap}
@@ -41,26 +44,28 @@ export const Collaborators = ({data}:{data?:string[]}) => {
       viewport={{ once: true }}
       transition={{ duration: 0.8, delay: 0.5 }}
     >
-    <Stack gap={'clamp(2rem, 4vw, 4rem)'} className={classes.collaboratorsWrapper}>
-      {
-        data ? <></> : <Stack style={{gap: 'clamp(1rem, 2vw, 2rem)'}} align='center'>
-          <Title ta={'center'} order={2} fz={{0: 40, md: 54}} className={classes.collaboratorsTitle}>
-            Powerful Crypto <Text span className={classes.collaboratorsTitleHighlighted}>Collaborations</Text>
-          </Title>
-          <Text ta="center" fz={{0: 16, md: 20}} className={classes.collaboratorsSubTitle}>
-            Powering Progress Together: Explore Our Dynamic Network of Strategic Alliances <br/> and Collaborations in the Crypto World.
-          </Text>
-        </Stack>
-      }
-      <Box className={classes.collaboratorsCarouselWrapper}>
-        <div className={classes.scroller} data-animated='true'>
-          <ul className={clsx(classes.imagesList, classes.scrollerInner)}>
-            {collaboratorsSlides}
-          </ul>
-        </div>
-      </Box>
-
-    </Stack>
+      <Stack gap={"clamp(2rem, 4vw, 4rem)"} className={classes.collaboratorsWrapper}>
+        {data ? (
+          <></>
+        ) : (
+          <Stack style={{ gap: "clamp(1rem, 2vw, 2rem)" }} align="center">
+            <Title ta={"center"} order={2} className={classes.collaboratorsTitle}>
+              Powerful Crypto{" "}
+              <Text span className={classes.collaboratorsTitleHighlighted}>
+                Collaborations
+              </Text>
+            </Title>
+            <Text ta="center" variant="text-2" className={classes.collaboratorsSubTitle}>
+              Powering Progress Together: Explore Our Dynamic Network of Strategic Alliances and Collaborations in the Crypto World.
+            </Text>
+          </Stack>
+        )}
+        <Box className={classes.collaboratorsCarouselWrapper}>
+          <div className={classes.scroller} data-animated="true">
+            <ul className={clsx(classes.imagesList, classes.scrollerInner)}>{collaboratorsSlides}</ul>
+          </div>
+        </Box>
+      </Stack>
     </motion.div>
   );
 };
