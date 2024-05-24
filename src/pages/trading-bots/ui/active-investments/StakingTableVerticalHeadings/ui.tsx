928a16d25df91d38fcaed5561a8d641bf5205fb4
Khrystyna Derenivska <khrisderenivska@gmail.com>
2024-05-24 20:26:02 +0300

    active investments table - resize search input on screen width < 370px

--- src/pages/trading-bots/ui/active-investments/StakingTableVerticalHeadings/ui.tsx
+++ src/pages/trading-bots/ui/active-investments/StakingTableVerticalHeadings/ui.tsx
@@ -1,5 +1,7 @@
+import { getSiblings } from "@/helpers/getResponsivePaginationSiblings";
 import { Divider, Flex, Group, Pagination, Stack, Table, Text, TextInput, Title, rem } from "@mantine/core";
 import clsx from "clsx";
+import { useEffect, useState } from "react";
 
 import { NextIcon, PreviousIcon, SearchIcon } from "@/shared/ui";
 
@@ -7,6 +9,20 @@ import { INVESTMENTS_WITH_HEADINGS } from "../Investments";
 import classes from "./styles.module.css";
 
 export const StakingTableVerticalHeadings = () => {
+  const [siblings, setSiblings] = useState(getSiblings());
+
+  useEffect(() => {
+    const handleResize = () => {
+      setSiblings(getSiblings());
+    };
+
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
   return (
     <Stack className={classes.wrapper}>
       <Stack className={classes.box} gap={0}>
@@ -25,18 +41,19 @@ export const StakingTableVerticalHeadings = () => {
 
         <Divider size="xs" classNames={{ root: classes.ratesDividerRoot }} />
         <div className={classes.tableContainer}>
-          <Table classNames={{ tr: classes.tableTr, td: classes.tableTd }} verticalSpacing={rem("16px")} withRowBorders={true}>
+          <Table
+            classNames={{ tr: classes.tableTr, td: classes.tableTd, table: classes.verticalHedingsTable }}
+            verticalSpacing={rem("16px")}
+            withRowBorders={true}
+          >
             <Table.Tbody classNames={{ tbody: classes.tableBody }}>
               {INVESTMENTS_WITH_HEADINGS.map((investment, index) => (
                 <Table.Tr key={index}>
-                  <Table.Th w={"135"} className={classes.tableHeadingCell}>
-                    {investment.heading}
-                  </Table.Th>
+                  <Table.Th className={classes.tableHeadingCell}>{investment.heading}</Table.Th>
                   {investment.coins.map((coin) => {
                     if (typeof coin !== "object") {
                       return (
                         <Table.Td
-                          w={"275"}
                           className={clsx(
                             classes.tableCellWithLeftIndent,
                             { [classes.linkButton]: coin === "Stop" },
@@ -48,7 +65,7 @@ export const StakingTableVerticalHeadings = () => {
                       );
                     } else {
                       return (
-                        <Table.Td w={"225"} px={15} className={classes.tbodyTdWithIcon}>
+                        <Table.Td px={15} className={classes.tbodyTdWithIcon}>
                           <Group gap={rem(8)}>
                             {coin.icon}
                             <Title c="white" order={4}>
@@ -70,7 +87,7 @@ export const StakingTableVerticalHeadings = () => {
           <Text variant="text-4" className={classes.greyText}>
             1-20 of 9,383 assets
           </Text>
-          <Pagination total={20} defaultValue={1}>
+          <Pagination total={20} defaultValue={1} {...{ siblings }}>
             <Group gap={0} justify="center">
               <Pagination.Previous icon={PreviousIcon} />
               <Pagination.Items />
