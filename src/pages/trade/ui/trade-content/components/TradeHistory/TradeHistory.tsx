fc03cf0a65ec231929d31dfc3ca9109aa36d93bb
Khrystyna Derenivska <khrisderenivska@gmail.com>
2024-05-25 21:33:00 +0300

    add horizontal scrollbar to  trade history tables

--- src/pages/trade/ui/trade-content/components/TradeHistory/TradeHistory.tsx
+++ src/pages/trade/ui/trade-content/components/TradeHistory/TradeHistory.tsx
@@ -44,3 +44,29 @@ export const TradeHistory = () => {
     </Container>
   );
 };
+
+{
+  /* <Container padding={48}>
+<Stack gap={32}>
+  <Group justify="space-between">
+    <Text className={classes.tradeHistoryBigText}>Trade History</Text>
+    <Group gap={8}>
+      <CalendarIcon />
+      <Select defaultFirst backgroundTransparent activeValue={activePeriodValue} setActiveValue={setActivePeriodValue} />
+    </Group>
+  </Group>
+  <Tabs tabs={tabs} />
+  <Divider color="white" opacity={0.12} />
+  <Group justify={"space-between"}>
+    <Text className={classes.grayText}>1-20 of 9,383 assets</Text>
+    <Pagination total={20} defaultValue={1}>
+      <Group gap={8} justify="center">
+        <Pagination.Previous icon={PreviousIcon} />
+        <Pagination.Items />
+        <Pagination.Next icon={NextIcon} />
+      </Group>
+    </Pagination>
+  </Group>
+</Stack>
+</Container> */
+}
