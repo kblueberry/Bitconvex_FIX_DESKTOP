7ecf94b72c9e3e23e42a3ff96babe83c449707ba
Khrystyna Derenivska <khrisderenivska@gmail.com>
2024-05-26 13:28:33 +0300

    add title for payment section & fixed payment container width on mobile screens

--- src/pages/trade/ui/trade-content/components/Payment/Payment.tsx
+++ src/pages/trade/ui/trade-content/components/Payment/Payment.tsx
@@ -1,12 +1,11 @@
 import { useResize } from "@/hooks/useResize";
-import { Group, Stack, Text } from "@mantine/core";
+import { Stack } from "@mantine/core";
 
-import { Coins } from "@/shared/ui/Coins/Coins";
+import { TradeChartTitle } from "@/shared/ui/TradeChartTitle";
 import { Container } from "@/shared/ui/TradePageContainer/Container";
 import { Tabs } from "@/shared/ui/tabs";
 
 import containerClasses from "../../../../../../shared/ui/TradePageContainer/Container.module.css";
-import classes from "../TradeChart/TradeChart.module.css";
 import { BuyTab } from "./components/tabs/BuyTab/BuyTab";
 import { SellTab } from "./components/tabs/SellTab/SellTab";
 
@@ -20,14 +19,7 @@ export const Payment = () => {
   return (
     <Stack className={containerClasses.payment}>
       <Container style={{ padding: "2rem clamp(1.5rem, 2vw, 2rem) 2rem clamp(1.5rem, 2vw, 2rem)" }}>
-        {md && (
-          <>
-            <Group gap={8} className={classes.tradeChartWrapper}>
-              <Text className={classes.tradeChartTitle}>Bitcoin/USDT Chart</Text>
-              <Coins />
-            </Group>
-          </>
-        )}
+        {md && <TradeChartTitle className="withBottomIndent" />}
 
         <Tabs overflowContainer={false} tabs={tabs} />
       </Container>
